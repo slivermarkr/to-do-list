1dcb23e02e9e562e97d773712252ccc6f1cf93f7
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-05-13 22:39:36 +0800

    try

--- sample/resources/script.js
+++ sample/resources/script.js
@@ -25,3 +25,26 @@ class Task {
   this.status = status
  }
 }
+
+function InputController() {
+ const LIST = new List();
+
+ const addProjectTitle = (title) => {
+  LIST.addProject(title);
+ }
+
+ const printProjectList = () => {
+  console.log(LIST.PROJECTLIST)
+ }
+
+ const addTaskInfo = (projectIndex,taskName,taskDesc,taskStatus) => {
+  LIST.PROJECTLIST[projectIndex].addTask(taskName,taskDesc,taskStatus);
+  printProjectList();
+ }
+
+ return {
+  LIST,
+  addProjectTitle,
+  addTaskInfo
+ }
+}
