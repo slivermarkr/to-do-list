02fb72d2789a710f999160905f02cccb4e0d4049
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-05-20 12:37:22 +0800

    fix the bug on default project bug

--- src/classes/task.js
+++
@@ -1,8 +0,0 @@
-export default class Task {
- constructor(name,description,priority,date) {
-  this.taskName = name
-  this.taskDescriptioin = description
-  this.taskPriorityLevel = priority
-  this.taskDeadline = date
- }
-}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,59 @@
-import setFunc from './components/main.js'
+import SetupController from './components/setup'
 
-const me = setFunc()
-console.log(me.getLIST());
-console.log(me.getProjectLIST());
+
+
+function sayHi() {
+ console.log('Hello there')
+}
+
+function InputController() {
+ const LIST = SetupController();
+ 
+ LIST.loadFromLocalStorage()
+
+ let currentIndex;
+ const getCurrentIndex = () => currentIndex;
+
+ const getProjectTitle = (title) => {
+  LIST.createProjectInstance(title)
+ }
+
+ const createDefaultProject = () => {
+  const projectInStrings = localStorage.getItem('projects');
+  if(!projectInStrings){
+  LIST.createProjectInstance('Today')
+  LIST.createProjectInstance('Home')
+  LIST.createProjectInstance('Work')
+ }
+ }
+
+ const addProject = document.querySelector('#addProject');
+ const titleDialog = document.querySelector('#titleDialog');
+ const title = document.querySelector('#title');
+ const addTitle = document.querySelector('#confirmTitle');
+ const projectListDiv =  document.querySelector('.projects-list ul');
+ const lists = document.querySelectorAll('.projects-list ul li');
+ 
+ projectListDiv.textContent = ""
+
+ addProject.onclick = () => {
+  titleDialog.showModal()
+ }
+ 
+ title.addEventListener('change', (e) => {
+  addTitle.value = title.value;
+  console.log(addTitle.value)
+ })
+ 
+ titleDialog.addEventListener('close', (e) => {
+  getProjectTitle(titleDialog.returnValue);
+
+ });
+ addTitle.addEventListener('click', (e) => {
+  e.preventDefault();
+  titleDialog.close(title.value);
+ })
+
+ createDefaultProject()
+}
+const me = InputController()
