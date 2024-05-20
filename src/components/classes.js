2a75d1b7c287224aecdf2e6e0f52d4c92421997e
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-05-20 12:20:13 +0800

    local storage finally make some sense

---
+++ src/components/classes.js
@@ -0,0 +1,33 @@
+export default class List {
+ constructor() {
+  this.PROJECTLIST = []
+ }
+ addProject(title) {
+  const project = new Project(title);
+  this.PROJECTLIST.push(project);
+  this.saveToLocalStorage()
+ }
+ saveToLocalStorage() {
+  localStorage.setItem("projects",JSON.stringify(this.PROJECTLIST));
+ }
+}
+
+export class Project {
+ constructor(projectTitle) {
+  this.title = projectTitle
+  this.TASKLIST = []
+ }
+ addTask(taskName,taskDescription,taskPriorityLevel,taskDeadline) {
+  const task = new Task(taskName,taskDescription,taskPriorityLevel,taskDeadline)
+  this.TASKLIST.push(task);
+ }
+}
+
+export class Task {
+ constructor(taskName,taskDescription,taskPriorityLevel,taskDeadline) {
+  this.name = taskName
+  this.description = taskDescription
+  this.priorityLevel = taskPriorityLevel
+  this.deadline = taskDeadline
+ }
+}
---
+++ src/components/setup.js
@@ -0,0 +1,39 @@
+import List from './classes.js' 
+
+export default function SetupList() {
+ const LIST = new List()
+
+ const getListOfProject = () => LIST.PROJECTLIST;
+ 
+ const printListOfProject = () =>  console.log(LIST.PROJECTLIST);
+
+ const createProjectInstance = (title) => {
+  LIST.addProject(title);
+  printListOfProject()
+ }
+
+ const createTaskInstance = (index,name,description,priorityLevel,date) => {
+  getListOfProject()[index].addTask(name,description,priorityLevel,date);
+  LIST.saveToLocalStorage();
+ }
+
+ const loadFromLocalStorage = () => {
+  const projectInStrings= localStorage.getItem('projects');
+
+  if(projectInStrings) {
+   const parsedProjects = JSON.parse(projectInStrings);
+   parsedProjects.forEach(project => {
+    createProjectInstance(project.title)
+    project.TASKLIST.forEach(task => {
+    createTaskInstance(task.name,task.description,task.priorityLevel,task.deadline);
+    });
+   });
+  };
+ };
+ return {
+  getListOfProject,
+  createProjectInstance,
+  createTaskInstance,
+  loadFromLocalStorage,
+ }
+} 
